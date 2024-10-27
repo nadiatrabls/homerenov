58143179d7ae3c49922de961b8ac92a4d0d10b76
Nadia <megdiche.nadia1996@gmail.com>
2024-10-27 16:24:12 +0100

    formulaire

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ContactFormType;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
@@ -24,5 +26,33 @@ class ContactController extends AbstractController
         return $this->render('contact/index.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+
+
+    public function contact(Request $request, MailerInterface $mailer): Response
+    {
+        if ($request->isMethod('POST')) {
+            $name = $request->request->get('name');
+            $email = $request->request->get('email');
+            $messageContent = $request->request->get('message');
+
+            // Créer l'email
+            $emailMessage = (new Email())
+                ->from($email)
+                ->to('contact@homerenov91.fr') // Votre adresse email IONOS
+                ->subject('Nouveau message de contact')
+                ->text("Nom: $name\nEmail: $email\nMessage:\n$messageContent");
+
+            // Envoyer l'email
+            $mailer->send($emailMessage);
+
+            // Optionnel : ajouter un message flash de confirmation
+            $this->addFlash('success', 'Votre message a bien été envoyé.');
+
+            return $this->redirectToRoute('contact_page');
+        }
+
+        return $this->render('home/index.html.twig');
     }
 }
